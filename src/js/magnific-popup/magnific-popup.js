cae57572732cd5531ef3177b45ad2519d28591eb
= <=>
2019-02-03 02:53:21 -0200

    magnific popup implementation

--- src/js/magnific-popup/magnific-popup.js
+++
@@ -1,8 +0,0 @@
-// This will create a single gallery from all elements that have class "gallery-item"
-$('.gallery-item').magnificPopup({
-    type: 'image',
-    delegate: 'a',
-    gallery:{
-      enabled:true
-    }
-  });
